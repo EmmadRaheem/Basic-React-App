8e06683c1fc643d41f16c944550a16a143c5042e
EmmadRaheem <EmmadRaheem@users.noreply.github.com>
2020-06-07 02:04:43 +0500

    Create Basic React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hello from './Hello.js'
+import './App.css';
+
+const App = ({name}) => {
+  return <div>
+            <h2>Assignment # 03</h2>
+            <h3>Completed by {name}<hr /></h3>
+            <p>This paragraph is written in App.js <br /> 
+            It's a coustom component of index.js which we call <strong>tag</strong> in html language<br />
+            I have used <i>function into an arrow method</i> to write a function
+            <hr />
+            </p>
+            <Hello></Hello>
+          </div>
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Hello.css';
+
+function hello() {
+  return <div className="Hello">
+            This is my 2nd coustom component I used in this project<br />
+            I have used <i>plain function method</i> to write a function<br />
+         </div>
+}
+
+export default hello;
