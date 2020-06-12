06d159403b6e1d0ff7c08b720aa25d034132c10f
EmmadRaheem <EmmadRaheem@users.noreply.github.com>
2020-06-12 13:44:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Hello from './Hello.js'
 import './App.css';
 
-const App = ({name}) => {
+const App = ({my_name}) => {
   return <div>
             <h2>Assignment # 03</h2>
-            <h3>Completed by {name}<hr /></h3>
+            <h3>Completed by {my_name}<hr /></h3>
             <p>This paragraph is written in App.js <br /> 
             It's a coustom component of index.js which we call <strong>tag</strong> in html language<br />
             I have used <i>function into an arrow method</i> to write a function
